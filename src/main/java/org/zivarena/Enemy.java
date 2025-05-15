48376e0894faa9dd166b792b5988f4e7b79914d9
realzvqle <realzvqle@gmail.com>
2025-05-14 21:44:26 -0500

    push

---
+++ src/main/java/org/zivarena/Enemy.java
@@ -0,0 +1,28 @@
+package org.zivarena;
+
+import static com.raylib.Raylib.*;
+
+public class Enemy extends Object {
+    private float speed = 200;
+    public Enemy(float positionX, float positionY, float size, Color color) {
+        super(positionX, positionY, size, size, color);
+    }
+    public void UpdateEnemy(Vector2 playerPos){
+        UpdatePosition(playerPos);
+        super.RenderObject();
+    }
+    public void UpdatePosition(Vector2 playerPos){
+        if(super.position.x() - playerPos.x() > 25){
+            super.position.x(super.position.x() - 200 * GetFrameTime());
+        }
+        else if(super.position.x() - playerPos.x() < -25){
+            super.position.x(super.position.x() + 200 * GetFrameTime());
+        }
+        if(super.position.y() - playerPos.y() > 25){
+            super.position.y(super.position.y() - 200 * GetFrameTime());
+        }
+        else if(super.position.y() - playerPos.y() < -25){
+            super.position.y(super.position.y() + 200 * GetFrameTime());
+        }
+    }
+}
--- src/main/java/org/zivarena/Game.java
+++ src/main/java/org/zivarena/Game.java
@@ -1,20 +1,24 @@
 package org.zivarena;
 
 import static com.raylib.Colors.BLACK;
+import static com.raylib.Colors.RED;
 import static com.raylib.Raylib.ClearBackground;
 import static com.raylib.Raylib.CloseWindow;
 
 public class Game {
     private final Window window;
     Player player;
+    Enemy enemy;
     public Game(){
         window = new Window(1600, 900, "Zvqles Arena");
         player = new Player(window.w() / 2, window.h() / 2, 25);
+        enemy = new Enemy(window.w() / 3, window.h() / 3, 25, RED);
     }
     public void StartGameLoop(){
         while(window.WindowLoop()){
             ClearBackground(BLACK);
             player.UpdatePlayer();
+            enemy.UpdateEnemy(player.position);
             window.Swap();
         }
     }
